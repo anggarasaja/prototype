1e7037cbb7e39c9de798927825baa0849e45f70e
anggarasaja <harrisanggara@gmail.com>
2014-12-23 20:30:11 +0700

    --

--- cms/view/foot.php
+++ cms/view/foot.php
@@ -1,6 +1,6 @@
 	
 	    <!-- SB Admin Scripts - Include with every page -->
-    <script src="<?=$url_rewrite?>js/sb-admin.js"></script>
+    <script src="<?=$url_rewrite?>js/sb-admin-2.js"></script>
 <script type="text/javascript" language="javascript" src="../view/media/js/jquery.dataTables.js"></script>
 
-<script src="<?=$url_rewrite?>js/plugins/metisMenu/jquery.metisMenu.js"></script>
+<script src="<?=$url_rewrite?>js/plugins/metisMenu/metisMenu.js"></script>
